a87cd0d5f0c4445bb18195db68df3fade68b6577
Paul Davies <lagdotcom@gmail.com>
2020-01-08 15:03:25 +0000

    debug button for generating floors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import Game from './Game';
 
 window.addEventListener('load', () => {
     let game = new Game(document.body);
-
     (<any>window).G = game;
+
+    let genFloorBtn = document.createElement('button');
+    genFloorBtn.innerText = 'Generate Floor';
+    genFloorBtn.addEventListener('click', () => {
+        game.showAll(
+            game.architect.generate(
+                game.display.width,
+                game.display.height,
+                200,
+            ),
+        );
+    });
+    document.body.append(genFloorBtn);
 });
