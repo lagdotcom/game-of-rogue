e10ec0c76e6c5223603c4ac833438e19660908bc
Paul Davies <lagdotcom@gmail.com>
2021-12-29 12:02:45 +0000

    made rooms loader more elegant

--- src/rooms.ts
+++ src/rooms.ts
@@ -1,13 +1,3 @@
-import ambushYard from '../res/room/ambush-yard.txt';
-import bend from '../res/room/bend.txt';
-import grandHall from '../res/room/grand-hall.txt';
-import guardedVault from '../res/room/guarded-vault.txt';
-import junction from '../res/room/junction.txt';
-import longCorridor from '../res/room/long-corridor.txt';
-import smallTreasure from '../res/room/small-treasure.txt';
-import tJunction from '../res/room/t-junction.txt';
-import tinyPillarHall from '../res/room/tiny-pillar-hall.txt';
-import yard from '../res/room/yard.txt';
 import { Grid } from './Grid';
 
 function room(name: string, s: string) {
@@ -20,15 +10,5 @@ function room(name: string, s: string) {
     return g;
 }
 
-export const rooms = [
-    ['ambush-yard', ambushYard],
-    ['bend', bend],
-    ['grand-hall', grandHall],
-    ['guarded-vault', guardedVault],
-    ['junction', junction],
-    ['long-corridor', longCorridor],
-    ['small-treasure', smallTreasure],
-    ['t-junction', tJunction],
-    ['tiny-pillar-hall', tinyPillarHall],
-    ['yard', yard],
-].map((a) => room(a[0], a[1]));
+const roomTxt = require.context('../res/room', true, /\.txt$/);
+export const rooms = roomTxt.keys().map((k) => room(k, roomTxt(k).default));
